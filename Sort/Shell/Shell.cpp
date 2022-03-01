503f1fc95c91dea6c0de8c53e1d3a378edf8330d
Dima Kachkovskii <90241790+MACTEPOB@users.noreply.github.com>
2022-03-01 16:31:13 +0500

    Add files via upload

---
+++ Sort/Shell/Shell.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int n, d, j, g, c;
+	srand(time(NULL));
+	cout << "Задайте размер массива" << endl;
+	cin >> n;
+	cout << "Задайте минимальную и максимальную границы массива" << endl; 
+	cin >> c >> g;
+	int* a = new int[n];
+	g--;
+	cout << "Default sequence:";
+	for (int i = 0; i < n; i++)
+	{
+		a[i] = rand() % g + c;
+		cout << " " << a[i];
+	}
+	cout << endl;
+	d = n/2;
+	while (d > 0)
+	{
+		for (int i = 0; i < n - d; i++)
+		{
+			j = i;
+			while (j >= 0 and a[j] > a[j + d])
+			{
+				swap(a[j], a[j + d]);
+				j--;
+			}
+		}
+		d = d / 2;
+	}
+	cout << "Sorted sequence:";
+	for (int i = 0; i < n; i++)
+		cout << " " << a[i];
+	return 0;
+}
