1a053aaafd79a4d648c16c135943fc16a5a2ca88
Dima Kachkovskii <90241790+MACTEPOB@users.noreply.github.com>
2022-03-10 00:37:35 +0500

    Add files via upload

---
+++ HanoiTower/HanoiTower.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+void hanoi(int num, char from, char to, char buff)
+{
+    if (num != 0)
+    {
+        hanoi(num - 1, from, buff, to);
+        cout << from << " -> " << to << endl;
+        hanoi(num - 1, buff, to, from);
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    int num;
+    cout << "Введите количество колец: ";
+    cin >> num;
+    cout << endl;
+    hanoi(num, '1', '2', '3');
+    return 0;
+}
