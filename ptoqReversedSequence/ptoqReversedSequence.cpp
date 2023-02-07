2dfb9a9fc35449dc50e3e684deb958b0b415194c
Dima Kachkovskii <90241790+MACTEPOB@users.noreply.github.com>
2023-02-07 21:48:04 +0500

    Add files via upload

---
+++ ptoqReversedSequence/ptoqReversedSequence.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    const int n = 10;
+    int a[n];
+    srand(time(NULL));
+    cout << "Default sequence:";
+    for (int i = 0; i <= 9; i++)
+    {
+        a[i] = rand() % 101;
+        cout << " " << a[i];
+    }
+    cout << endl;
+    int p, q;
+    cout << "Задайте индексы p и q\n";
+    cin >> p >> q;
+    while (p != q && p<q)
+    {
+        swap(a[p], a[q]);
+        p++;
+        q--;
+    }
+    cout << "Reversed sequence:";
+    for (int i = 0; i <= 9; i++)
+    {
+        cout << " " << a[i];
+    }
+    return 0;
+}
